b2d8605aa58d0ac709c04304249999249c3a3355
Marcin Jachymiak <marcin.jachymiak1@gmail.com>
2019-01-17 14:08:21 -0500

    Move comment about BaseIndex::DB from TxIndex::DB

--- src/index/txindex.cpp
+++ src/index/txindex.cpp
@@ -41,15 +41,7 @@ struct CDiskTxPos : public CDiskBlockPos
     }
 };
 
-/**
- * Access to the txindex database (indexes/txindex/)
- *
- * The database stores a block locator of the chain the database is synced to
- * so that the TxIndex can efficiently determine the point it last stopped at.
- * A locator is used instead of a simple hash of the chain tip because blocks
- * and block index entries may not be flushed to disk until after this database
- * is updated.
- */
+/** Access to the txindex database (indexes/txindex/) */
 class TxIndex::DB : public BaseIndex::DB
 {
 public:
